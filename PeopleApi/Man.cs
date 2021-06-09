ae9e6259d682bdd95953e1d27a144ca4c80e27b9
KarlMarx-spec <55876912+KarlMarx-spec@users.noreply.github.com>
2021-06-09 17:24:29 +0300

    Add files via upload

---
+++ PeopleApi/Man.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeopleApi.Models
+{
+    public class Man
+    {
+        public long Id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+}
---
+++ PeopleApi/PeopleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeopleApi.Models
+{
+    public class PeopleContext : DbContext
+    {
+        public DbSet<Man> Men { get; set; }
+        public PeopleContext(DbContextOptions<PeopleContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // creating Data Base
+        }
+        
+    }
+}
